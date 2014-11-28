633f0449eb13f039ac996e4a719f310221a11f5f
Sim <pingping.sim83@gmail.com>
2014-11-29 04:17:44 +0800

    initial commit of healthylicious app
    - main screen listing of recipes loaded from yummly restful api.

---
+++ src/com/mobile/healthylicious/recipe/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mobile.healthylicious.recipe;
+
+import org.apache.cordova.Config;
+import org.apache.cordova.DroidGap;
+
+import android.os.Bundle;
+
+public class MainActivity extends DroidGap {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		super.loadUrl(Config.getStartUrl());
+	}
+}
